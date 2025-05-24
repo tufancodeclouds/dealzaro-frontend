5665283e3062a293e376a223651a7bd764605e7c
Tufan Ghosh <tufan.ghosh@codeclouds.co.in>
2025-05-24 20:28:38 +0530

    fixing reload issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { createRoot } from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </HashRouter>,
+  </BrowserRouter>,
 )
