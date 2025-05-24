d39b451b8c14d7c304999bf06ac8ba219c5ff66e
Tufan Ghosh <tufan.ghosh@codeclouds.co.in>
2025-05-24 19:55:48 +0530

    BrowserRouter change to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { createRoot } from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 )
