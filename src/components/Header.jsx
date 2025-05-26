d0189b6c1b419a1d038cd9d431a5fc9d3c74cd6e
Tufan Ghosh <tufan.ghosh@codeclouds.co.in>
2025-05-26 10:30:21 +0530

    some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
     }
 
   return (
-    <header className={`flex items-center justify-between py-5 font-medium sticky top-0 bg-white z-50 ${showBorder ? 'border-b' : ''}`}>
+    <header className={`flex items-center justify-between py-5 font-medium relative sm:sticky top-0 bg-white z-50 ${showBorder ? 'border-b' : ''}`}>
       
       <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
 
@@ -86,7 +86,7 @@ const Header = () => {
       </div>
 
         {/* Sidebar menu for small screens */}
-        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all z-[2] ${visible ? 'w-full' : 'w-0'}`}>
+        <div className={`fixed top-0 right-0 bottom-0 overflow-hidden bg-white transition-all z-[2] ${visible ? 'w-full' : 'w-0'}`}>
                 <div className='flex flex-col text-gray-600'>
                     <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
                         <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
